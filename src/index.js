5be38b1418deb237ad27de8be2eb6c1e84576e69
Yago <jagogc@gmail.com>
2018-01-21 16:33:22 +0000

    App component created

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import { render } from 'react-dom';
 import './css/style.css';
 
 import StorePicker from './components/StorePicker'
+import App from './components/App';
 
 render(<StorePicker/>, document.querySelector('#main'));
