c1168ec3f1e9f06aad7bd7a92ca9afa0c9366ab4
Yago <jagogc@gmail.com>
2018-01-23 21:01:50 +0000

    Added Fish Form

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,20 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			fishes: {},
+			order: {}
+		}
+	}
+	addFish(fish) {
+		const fishes = {...this.state.fishes};
+		const timestamp = Date.now();
+		fishes[`<fish-${timestamp}`] = fish;
+
+		this.setState({fishes: fishes});
+	}
 	render() {
 		return (
 			<div className="catch-of-the-day">
